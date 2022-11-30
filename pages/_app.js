a698e854ca0ceeb3c8d75c37bd1b6b8732af4178
Ryan Flores <ryan@tecobi.com>
2022-11-30 10:51:35 -0800

    Revert "import bootstrap from npm instead of pasted into app.css"
    This reverts commit 1614c4a3d679abf88ed577002188c6ec73173f26.

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,3 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
 import '../public/assets/css/app.css'
 import "../public/assets/css/swiper-custom.css";
 import React, { useEffect, useState } from "react";
