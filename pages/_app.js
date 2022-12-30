e1c626eeacab297a1f1e513214c2e33e9689f938
Ryan Flores <ryan@tecobi.com>
2022-12-30 14:47:29 -0800

    add google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,10 @@
-import '../public/assets/css/app.css'
+import "../public/assets/css/app.css";
 import "../public/assets/css/swiper-custom.css";
 import React, { useEffect, useState } from "react";
-import Preloader from '../components/elements/Preloader';
-import Script from 'next/script'
+import Preloader from "../components/elements/Preloader";
+import Script from "next/script";
 
-import 'react-modal-video/css/modal-video.css';
+import "react-modal-video/css/modal-video.css";
 function MyApp({ Component, pageProps }) {
   const [loading, setLoading] = useState(false);
   useEffect(() => {
@@ -21,15 +21,32 @@ function MyApp({ Component, pageProps }) {
   }, []);
   return (
     <>
-      <Script strategy="beforeInteractive" src="https://www.paypal.com/sdk/js?client-id=AWMXc5D6QQkOEeN2FGCMBGSsraqL6kJMWCnJHPv-MT3Y3lM9mIoTQ4SX6hLnQhAnkGVzPKsvzolzGUY1&vault=true&intent=subscription" data-sdk-integration-source="button-factory" />
-      {!loading ? (
-        <Component {...pageProps} />
-      ) : (
-        <Preloader />
-      )}
-
+      <Script
+        strategy="beforeInteractive"
+        src="https://www.paypal.com/sdk/js?client-id=AWMXc5D6QQkOEeN2FGCMBGSsraqL6kJMWCnJHPv-MT3Y3lM9mIoTQ4SX6hLnQhAnkGVzPKsvzolzGUY1&vault=true&intent=subscription"
+        data-sdk-integration-source="button-factory"
+      />
+      {!loading ? <Component {...pageProps} /> : <Preloader />}
+      {/* Global Site Tag (gtag.js) - Google Analytics */}
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=G-PFR969PXSX`}
+      />
+      <Script
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-PFR969PXSX', {
+              page_path: window.location.pathname,
+            });
+          `,
+        }}
+      />
     </>
-  )
+  );
 }
 
-export default MyApp
+export default MyApp;
